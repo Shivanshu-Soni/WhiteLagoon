cf325adf9555ae124c0432a4ca1533b895d72a41
shivanshusoni1 <shivanshu.soni@celusion.com>
2025-10-22 00:57:38 +0530

    changes on import

---
+++ WhiteLagoon.Infrastructure/Migrations/20251021192235_Viila_model-Changed.cs
@@ -0,0 +1,60 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace WhiteLagoon.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class Viila_modelChanged : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.UpdateData(
+                table: "Villas",
+                keyColumn: "Id",
+                keyValue: 1,
+                columns: new[] { "Description", "Name" },
+                values: new object[] { "Premium Pool villa with attached bathroom", "Premium Pool Villa" });
+
+            migrationBuilder.UpdateData(
+                table: "Villas",
+                keyColumn: "Id",
+                keyValue: 2,
+                columns: new[] { "Description", "Name" },
+                values: new object[] { "Super premium villa", "Super Premium Villa " });
+
+            migrationBuilder.UpdateData(
+                table: "Villas",
+                keyColumn: "Id",
+                keyValue: 3,
+                columns: new[] { "Description", "Name" },
+                values: new object[] { "Presidential Suite villa for exotic experience", "Suite Villa" });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.UpdateData(
+                table: "Villas",
+                keyColumn: "Id",
+                keyValue: 1,
+                columns: new[] { "Description", "Name" },
+                values: new object[] { "Villa 1 Description", "Villa 1" });
+
+            migrationBuilder.UpdateData(
+                table: "Villas",
+                keyColumn: "Id",
+                keyValue: 2,
+                columns: new[] { "Description", "Name" },
+                values: new object[] { "Villa 2 Description", "Villa 2" });
+
+            migrationBuilder.UpdateData(
+                table: "Villas",
+                keyColumn: "Id",
+                keyValue: 3,
+                columns: new[] { "Description", "Name" },
+                values: new object[] { "Villa 3 Description", "Villa 3" });
+        }
+    }
+}
