4bed6b58b22d0dde3f0d8e86f3f22f1b6eea65c7
shivanshusoni1 <shivanshu.soni@celusion.com>
2025-10-22 03:27:32 +0530

    added model validations

--- WhiteLagoon.Domain/Entities/Villa.cs
+++ WhiteLagoon.Domain/Entities/Villa.cs
@@ -1,5 +1,7 @@
 using System;
 using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -8,12 +10,17 @@ namespace WhiteLagoon.Domain.Entities
     public class Villa
     {
         public int Id { get; set; }
+
+        [MaxLength(50)]
         public required string Name { get; set; }
         public string? Description { get; set; }
+        [Display(Name= "Price per Night")]
+        [Range(1000, 20000)]
         public double Price { get; set; }
         public int SqFt { get; set; }
         public int Occupancy { get; set; }
 
+        [Display(Name ="Image Url")]
         public string? ImageUrl { get; set; }
         public DateTime? CreatedAt { get; set; }
         public DateTime? UpdatedAt { get; set; }
--- WhiteLagoon.Web/Controllers/VillaController.cs
+++ WhiteLagoon.Web/Controllers/VillaController.cs
@@ -5,36 +5,45 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
+using WhiteLagoon.Domain.Entities;
 using WhiteLagoon.Infrastructure.Data;
 using WhiteLagoon.Web.Models;
 
 namespace WhiteLagoon.Web.Controllers
 {
     [Route("[controller]")]
-    public class VillaController : Controller
+    public class VillaController(ILogger<VillaController> logger, ApplicationDbContext dbContext) : Controller
     {
-        private readonly ILogger<VillaController> _logger;
-        private readonly ApplicationDbContext _dbContext;
-
-        public VillaController(ILogger<VillaController> logger, ApplicationDbContext dbContext)
-        {
-            _dbContext = dbContext;
-            _logger = logger;
-        }
+        private readonly ILogger<VillaController> _logger = logger;
+        private readonly ApplicationDbContext _dbContext = dbContext;
 
         [HttpGet("Index")]
         public IActionResult Index()
         {
-            var villas = _dbContext.Villas.ToList();
+            var villas = _dbContext.Villas.ToList(); 
             return View(villas);
         }
 
-        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
-
-        [HttpGet("Error")]
-        public IActionResult Error()
+        public IActionResult Create()
         {
-            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+            return View();
         }
-    }
+        [HttpPost]
+        public IActionResult Create(Villa villa)
+        {
+            ArgumentNullException.ThrowIfNull(villa);
+            if (!ModelState.IsValid)
+            {
+                ModelState.AddModelError("Name", "Name must not be empty");
+            }
+            else 
+            {
+                _dbContext.Villas.Add(villa);
+                _dbContext.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(villa);
+        }
+
+        }
 }
